beb1cb9ef588df91ae7494c7659dcc52c8c10b70
Alaska-999 <kristinagaranchuk@gmail.com>
2022-07-02 18:17:38 +0300

    Files uploading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //состояние отвечает за то перенесен ли файл в область загрузки или нет
+    const [drag, setDrag] = useState(false)
+
+    function dragStartHandler(e) {
+        e.preventDefault()
+        setDrag(true)
+    }
+
+    function dragLeaveHandler(e) {
+        e.preventDefault()
+        setDrag(false)
+    }
+
+    function onDropHandler(e) {
+        e.preventDefault()
+        let files = [...e.dataTransfer.files]
+        console.log(files)
+        //отправление файлов на сервер
+        // const formData = new FormData()
+        // formData.append('file', files[0])
+        // formData.append('userId', 1)
+        // axios.post('url', formData)
+        setDrag(false)
+    }
+
+    return (
+        <div className="app">
+            {drag
+                ? <div
+                    className='drop-area'
+                    onDragStart={e => dragStartHandler(e)}
+                    onDragLeave={e => dragLeaveHandler(e)}
+                    onDragOver={e => dragStartHandler(e)}
+                    onDrop={e => onDropHandler(e)}
+                >
+                    Release file to upload
+            </div>
+                : <div
+                    onDragStart={e => dragStartHandler(e)}
+                    onDragLeave={e => dragLeaveHandler(e)}
+                    onDragOver={e => dragStartHandler(e)}
+                >
+                    Drag file to upload
+                </div>
+            }
+        </div>
+    );
 }
 
 export default App;
